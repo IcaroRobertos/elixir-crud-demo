e1b863eb77f9359d14fc448127402dba7277b09d
Icaro Roberto <oi@icaroroberto.com>
2020-04-12 21:57:28 -0300

    Added Credo - Lint

--- mix.exs
+++ mix.exs
@@ -40,7 +40,8 @@ defmodule Manager.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:credo, "~> 1.3", only: [:dev, :test], runtime: false}
     ]
   end
 
